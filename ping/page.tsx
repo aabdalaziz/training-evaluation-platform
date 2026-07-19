01ab86a86e3bf43570c426ca38183b9780278d84
aabdalaziz <62610537+aabdalaziz@users.noreply.github.com>
2026-07-19 22:30:52 +0300

    Create page.tsx

---
+++ ping/page.tsx
@@ -0,0 +1,3 @@
+export default function Ping() {
+  return <div dir="rtl" style={{ fontFamily: "Tahoma", padding: 30, background: "#eef", minHeight: "100vh" }}><h1 style={{ color: "#c00" }}>PING-OK-V1</h1><p>إذا رأيت هذا النص فالنشر يعمل والخادم سليم.</p></div>;
+}
