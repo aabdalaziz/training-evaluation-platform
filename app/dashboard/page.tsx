9ba2af409e434207e6c63d1b2136b3b446fd977e
aabdalaziz <62610537+aabdalaziz@users.noreply.github.com>
2026-07-28 06:55:56 +0300

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
-import CommandCenter from "./components/sections/CommandCenter";
 'use client';
+
+import CommandCenter from "./components/sections/CommandCenter";
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
